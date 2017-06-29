4490185b266094b8b52afcab81d133e83d341ff5
Ivan Figueroa <ifigueroa@kinsolresearch.com>
2017-06-29 15:57:42 -0700

    changed the echo in the bat file

--- SampleClient/run.bat
+++ SampleClient/run.bat
@@ -1,7 +1,9 @@
+@echo off
 java ^
  -Djava.security.krb5.realm=ITC.INTRINSYC.COM ^
  -Djava.security.krb5.kdc=itc.intrinsyc.com ^
  -Djavax.security.auth.useSubjectCredsOnly=false ^
  -Djava.security.auth.login.config=bcsLogin.conf ^
- SampleClient ldap itc.intrinsyc.com 389
+ SampleClient ITC.INTRINSYC.COM WIN8SERVER 7878
  rem SampleClient <servicePrincipal> <hostName> <port >
+rem -Djava.security.krb5.conf=krb5.conf ^
